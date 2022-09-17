ceb45516f307822fdbeda0e234dc69130028ad61
martin-creator <martinlubowa@outlook.com>
2022-09-17 08:55:24 +0300

    Added notifications using toastr

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'bulma-extensions-rails', '~>1.0.30'
 
 gem 'devise', '~> 4.6'
 
+#gem 'toastr-rails', '~>1.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+global.toastr = require("toastr")
+// require("stylesheets/application.scss") = import "../stylesheets/application"
+
+// window.Noty = require("noty")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
