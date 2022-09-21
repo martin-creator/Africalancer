60e461b5f7ba0d902387393228cd8022c0c931f9
martin-creator <martinlubowa@outlook.com>
2022-09-21 20:09:04 +0300

    Created show offer of the request pages

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,9 +1,25 @@
 class OffersController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_offer, only: 
+    before_action :set_offer, only: [:accept, :reject]
     before_action :is_authorized, only: [:accept, :reject]
     def create
-        
+        req = Request.find(offer_params[:request_id])
+
+        if req && req.user_id == current_user.id 
+            redirect_to request.referrer, alert: "You cannot offer your own request"
+        end
+
+        if Offer.exists?(user_id: current_user.id, request_id: offer_params[:request_id])
+            redirect_to request.referrer, alert: "You can make only one offer at the moment"
+        end
+
+        @offer =  current_user.offers.build(offer_params)
+
+        if @offer.save
+            redirect_to request.referrer, notice: "Saved..."
+        else 
+            redirect_to request.referrer, flash: {error: @offer.errors.full_messages.join(', ')}
+        end
     end
 
     def accept
