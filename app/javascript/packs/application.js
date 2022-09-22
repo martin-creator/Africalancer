9978276cd01a5d0b866f91102d8662c60ae07e36
martin-creator <martinlubowa@outlook.com>
2022-09-22 15:49:42 +0300

    Added Review view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('raty-js')
+
 global.toastr = require("toastr")
 // require("stylesheets/application.scss") = import "../stylesheets/application"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_101701) do
+ActiveRecord::Schema.define(version: 2022_09_22_112817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -123,6 +123,21 @@ ActiveRecord::Schema.define(version: 2022_09_22_101701) do
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "review"
+    t.integer "stars", default: 1
+    t.uuid "order_id", null: false
+    t.bigint "gig_id"
+    t.bigint "buyer_id"
+    t.bigint "seller_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["buyer_id"], name: "index_reviews_on_buyer_id"
+    t.index ["gig_id"], name: "index_reviews_on_gig_id"
+    t.index ["order_id"], name: "index_reviews_on_order_id"
+    t.index ["seller_id"], name: "index_reviews_on_seller_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -155,4 +170,8 @@ ActiveRecord::Schema.define(version: 2022_09_22_101701) do
   add_foreign_key "pricings", "gigs"
   add_foreign_key "requests", "categories"
   add_foreign_key "requests", "users"
+  add_foreign_key "reviews", "gigs"
+  add_foreign_key "reviews", "orders"
+  add_foreign_key "reviews", "users", column: "buyer_id"
+  add_foreign_key "reviews", "users", column: "seller_id"
 end
