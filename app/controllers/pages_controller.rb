06006d2a2ade570dac699b8f2c18588bd2cef049
martin-creator <martinlubowa@outlook.com>
2022-09-23 08:00:48 +0300

    Created search filter for the search page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,9 @@ class PagesController < ApplicationController
     #@gigs = Gig.where("active = ? AND gigs.title ILIKE ? AND category_id = ?", true, "%#{params[:q]}%", params[:category] )
     
     @q = params[:q]
+    @min = params[:min]
+    @max = params[:max]
+    @delivery = params[:delivery].present? ? params[:delivery] : "0"
 
     query_condition = []
     query_condition[0] = "gigs.active = true"
@@ -23,6 +26,21 @@ class PagesController < ApplicationController
       query_condition.push  params[:category]
     end
 
-    @gigs = Gig.where(query_condition)
+    if !params[:min].blank?
+      query_condition[0] += " AND pricings.price >= ?"
+      query_condition.push  @min
+    end
+
+    if !params[:max].blank?
+      query_condition[0] += " AND pricings.price <= ?"
+      query_condition.push  @max
+    end
+
+    if !params[:delivery].blank? && params[:delivery] != "0"
+      query_condition[0] += " AND pricings.delivery_time <= ?"
+      query_condition.push  @delivery
+    end
+
+    @gigs = Gig.select("gigs.id, gigs.title, gigs.user_id, pricings.price AS price ").joins(:pricings).where(query_condition)
   end
 end
