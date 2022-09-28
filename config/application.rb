b91c92b456b093d310bdf4463faf52c599349c53
martin-creator <martinlubowa@outlook.com>
2022-09-28 19:22:52 +0300

    Added logic to handle online and offline status icon

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Africalancer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    #config.action_dispatch.cookies_same_site_protection = :none
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_dispatch.cookies_same_site_protection = :none
+
 end
