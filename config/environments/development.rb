719feb7554705b41a192d131f400761cf3623822
martin-creator <martinlubowa@outlook.com>
2022-10-04 10:57:57 +0300

    Created admin categories

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_dispatch.cookies_same_site_protection = :none
+  
+  config.hosts << /.+\.ngrok\.io/
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_04_064808) do
+ActiveRecord::Schema.define(version: 2022_10_04_072008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_10_04_064808) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active", default: true
   end
 
   create_table "gigs", force: :cascade do |t|
